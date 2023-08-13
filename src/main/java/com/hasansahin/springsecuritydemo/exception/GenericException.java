9651091c321ed0a853c9a2675c92ead4d65a6fbe
hasanssahin <sahinhhasann@gmail.com>
2023-08-13 15:12:29 +0300

    Create exception

---
+++ src/main/java/com/hasansahin/springsecuritydemo/exception/GenericException.java
@@ -0,0 +1,18 @@
+package com.hasansahin.springsecuritydemo.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class GenericException extends RuntimeException {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public GenericException(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
